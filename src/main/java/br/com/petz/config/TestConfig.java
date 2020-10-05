1f74103c54c85fbab14ded86e778bca7a6e61ed1
simonecarvalho <simone.carvalho@gmail.com>
2020-10-05 01:26:57 -0300

    Spring Security

--- src/main/java/br/com/petz/config/TestConfig.java
+++ src/main/java/br/com/petz/config/TestConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("test")
 public class TestConfig {
 	
 	@Autowired
---
+++ src/main/java/br/com/petz/security/AccountCredentials.java
@@ -0,0 +1,9 @@
+package br.com.petz.security;
+
+import lombok.Data;
+
+@Data
+public class AccountCredentials {
+    private String username;
+    private String password;
+}
